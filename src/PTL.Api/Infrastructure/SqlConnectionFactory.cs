f853efdf8cbcb6505db0f71d8c17122572f1d406
Fujale Ahmed Shaik <shaikfujale.ahmed@cognizant.com>
2026-09-13 19:31:53 +0100

    Add SQL Server connectivity and DB startup checks to PTL.Api

---
+++ src/PTL.Api/Infrastructure/SqlConnectionFactory.cs
@@ -0,0 +1,22 @@
+using System.Data;
+using Microsoft.Data.SqlClient;
+
+namespace PTL.Api.Infrastructure;
+
+public sealed class SqlConnectionFactory(IConfiguration configuration) : IDbConnectionFactory
+{
+    public IDbConnection CreateConnection()
+    {
+        var options = StartupChecks.RequireDatabaseOptions(configuration);
+        var connectionString = new SqlConnectionStringBuilder
+        {
+            DataSource = options.Host,
+            InitialCatalog = options.Name,
+            UserID = options.User,
+            Password = options.Password,
+            TrustServerCertificate = options.TrustServerCertificate
+        }.ConnectionString;
+
+        return new SqlConnection(connectionString);
+    }
+}
