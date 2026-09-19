fd097a40d11ab3a24b140a10212714be4ad42ddb
Fujale Ahmed Shaik <shaikfujale.ahmed@cognizant.com>
2026-09-19 23:43:32 +0100

    Fix post-login redirect to Customer controller to match intended landing page

--- src/PTL.InternalWeb/Features/Account/AccountController.cs
+++ src/PTL.InternalWeb/Features/Account/AccountController.cs
@@ -34,7 +34,7 @@ namespace PTL.InternalWeb.Features.Account
             if (!string.IsNullOrEmpty(model.ReturnUrl) && Url.IsLocalUrl(model.ReturnUrl))
                 return Redirect(model.ReturnUrl);
 
-            return RedirectToAction("Index", "Home");
+            return RedirectToAction("Index", "Customer");
         }
 
         [HttpPost]
